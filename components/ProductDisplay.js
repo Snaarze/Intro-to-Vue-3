981a409ea79203c7fe5da239b02754b889a996a5
Snaarze <panlasiguijeremyglenn49@gmail.com>
2025-02-17 16:20:02 +0800

    finished

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,11 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+        <button 
+        class="button" 
+        v-on:click="removeCart">
+        Remove Cart
+      </button>
       </div>
     </div>
   </div>`,
@@ -56,7 +61,10 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id )
+      },
+      removeCart(){
+        this.$emit('remove-to-card', this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
           this.selectedVariant = index
